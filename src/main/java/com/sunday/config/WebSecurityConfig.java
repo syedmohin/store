9e0da604a24c44e30467d2eb3a218f90370aae4a
syedmohin <786syedmohin@gmail.com>
2020-10-23 06:57:51 +0530

    Added Hello Controller

--- src/main/java/com/sunday/config/WebSecurityConfig.java
+++ src/main/java/com/sunday/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/login", "/register").permitAll()
+                .authorizeRequests().antMatchers("/login", "/register","/owner","/name").permitAll()
                 // all other requests need to be authenticated
                         .anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
